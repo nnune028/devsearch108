4e6e64b22578abdd6f92d4000e4f5c63ecf89950
Nicholas Nunez <nnune028@fiu.edu>
2023-08-03 00:39:52 -0400

    Create message form and add functionality.

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 from django.contrib.auth.decorators import login_required
 from .utils import searchProfiles, paginateProfiles
 
@@ -158,5 +158,45 @@ def deleteSkill(request, pk):
 
 @login_required(login_url='login')
 def inbox(request):
-    context = {}
+    profile = request.user.profile
+    messageRequests = profile.messages.all() # messages rather than message_set because of the related_name field
+    unreadCount = messageRequests.filter(is_read=False).count()
+    context = {'messageRequests':messageRequests, 'unreadCount':unreadCount}
     return render(request, 'users/inbox.html', context)
+
+
+@login_required(login_url='login')
+def viewMessage(request, pk):
+    profile = request.user.profile
+    message = profile.messages.get(id=pk)
+    if message.is_read == False:
+        message.is_read = True
+        message.save()
+    context = {'message':message}
+    return render(request, 'users/message.html', context)
+
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+    
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+            messages.success(request, 'Your message was successfully sent!')
+            
+    context = {'recipient':recipient, 'form':form}
+    return render(request, 'users/message_form.html', context)
