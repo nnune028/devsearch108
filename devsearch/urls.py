eae46a68a32b883328a94daaa024596fba863f47
Nicholas Nunez <nnune028@fiu.edu>
2023-08-03 22:57:09 -0400

    Add reset password templates and functionality.

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -18,12 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings # Allows us to connect to MEDIA_ROOT/MEDIA_URL
 from django.conf.urls.static import static # Allows us to create a URL for our static files
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"), name="reset_password_sent"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset_password.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset.html"), name="reset_password_complete"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
